06c5f307c499150b0afee1cb583066d805697873
sheharyar <isheharyarajmal18gmail.com>
2019-04-06 17:00:45 +0500

    minor issue

--- lib/three_for_two_deal.rb
+++ lib/three_for_two_deal.rb
@@ -6,7 +6,7 @@ class ThreeForTwoDeal
   def apply(items)
     selected_items = items.select{ |i| i.code == @code}
     if selected_items.size >= 3
-      selected_items.each_slice(2) do |item, item_free|
+      selected_items.each_slice(3) do |item, item_free|
         item_free.price = 0 unless item_free.nil?
       end
     end
